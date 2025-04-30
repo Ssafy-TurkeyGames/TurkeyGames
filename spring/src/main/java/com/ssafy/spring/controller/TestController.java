7404efa5599c8d8fc1861a4d0ee09ef1380bb7a5
KimDongHyeon <edwin.dkim@gmail.com>
2025-04-30 17:32:06 +0900

    Update spring/src/main/java/com/ssafy/spring/controller/TestController.java

--- spring/src/main/java/com/ssafy/spring/controller/TestController.java
+++ spring/src/main/java/com/ssafy/spring/controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 	@GetMapping("/")
 	public String hello(){
-		return "테스트2";
+		return "CICD테스트트";
 	}
 }
