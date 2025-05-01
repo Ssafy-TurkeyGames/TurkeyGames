570aa7e4ffd4ff05e3633ebe04bafe2de80b89c4
KimDongHyeon <edwin.dkim@gmail.com>
2025-05-01 18:35:05 +0900

    test: Webhook 작동 확인용 테스트 커밋2

--- spring/src/main/java/com/ssafy/spring/controller/TestController.java
+++ spring/src/main/java/com/ssafy/spring/controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 	@GetMapping("/")
 	public String hello(){
-		return "CICD테스트3";
+		return "CICD테스트4";
 	}
 }
