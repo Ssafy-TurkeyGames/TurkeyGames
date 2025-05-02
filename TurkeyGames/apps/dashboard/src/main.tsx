e30fd15a364926856283a28a6b2b6f89ec6ca080
김동현 <edwin.dkim@gmail.com>
2025-05-02 15:34:28 +0900

    S12P31E101-282 feat: Dockerfile 및 docker-compose.yml 작성

--- TurkeyGames/apps/dashboard/src/main.tsx
+++ TurkeyGames/apps/dashboard/src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+// 이 한 줄 추가 (Vite에서는 이렇게 환경 구분)
+const isProd = import.meta.env.MODE === 'production';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={isProd ? '/dashboard' : undefined}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
--- TurkeyGames/apps/dashboard/vite.config.ts
+++ TurkeyGames/apps/dashboard/vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// 환경에 따라 base 경로 분기
+const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: isProd ? '/dashboard/' : './', // 배포일 땐 절대경로, 개발일 땐 상대경로
   build: {
-    outDir: '../../dist/dashboard'
+    outDir: '../../dist/dashboard',
+    emptyOutDir: true,
   },
-})
+});
