b795215fef0b55fa41fe1619cecaa61c597434ee
유지웅 <ung03@naver.com>
2025-05-02 16:29:53 +0900

    S12P31E101-238 feat: 카메라 전환 기능 추가

--- aruco_marker/src/camera_test.py
+++ aruco_marker/src/camera_test.py
@@ -1,5 +1,8 @@
 import cv2
 
+cv2.namedWindow("Camera", cv2.WINDOW_NORMAL)  # 창 크기 조절 가능
+cv2.resizeWindow("Camera", 960, 540)          # 창 크기 지정
+
 # 카메라 초기화 (검색결과[2] 참조)
 cap = cv2.VideoCapture(0)
 
@@ -12,8 +15,18 @@ while True:
     # 화면 표시 (검색결과[4] 참조)
     cv2.imshow('Camera Test', frame)
     
-    # 'q' 키로 종료
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # 0번 카메라로 전환
+    if key == ord('0'):
+        cap.release()
+        current_cam = 0
+        cap = cv2.VideoCapture(current_cam)
+    # 1번 카메라로 전환
+    elif key == ord('1'):
+        cap.release()
+        current_cam = 1
+        cap = cv2.VideoCapture(current_cam)
+    # 종료
+    elif key == ord('q'):
         break
 
 # 자원 해제
