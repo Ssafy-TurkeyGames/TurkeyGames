cc1d94bab3d2aca56dcec77004f43297da82d95f
seowoo <dkr0210@naver.com>
2025-05-08 09:29:48 +0900

    S12P31E101-319 refactor: 게임보드 기본화면 컴포넌트화

--- TurkeyGames/apps/gameboard/src/pages/defaultPage/Default.tsx
+++ TurkeyGames/apps/gameboard/src/pages/defaultPage/Default.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import styles from './Default.module.css';
 import turkey from '../../assets/images/turkey.png'
+import SpinTurkey from '../../components/common/spinTurkey/SpinTurkey';
 
 export default function Default() {
   return (
     <div className={styles.layout}>
-      <div className={styles.turkey}>
-        <img src={turkey} />
-      </div>
+      <SpinTurkey image={turkey} />
     </div>
   )
 }
