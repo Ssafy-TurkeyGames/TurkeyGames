ddfce4aec6672ab84af0c92443540b5e09ab494a
kec0126 <kec0126@naver.com>
2025-05-09 12:42:02 +0900

    S12P31E101-330 design: 게임 결과 화면 css

--- TurkeyGames/apps/dashboard/src/routes/routes.tsx
+++ TurkeyGames/apps/dashboard/src/routes/routes.tsx
@@ -6,7 +6,7 @@ import Home from '../pages/Home';  // 홈화면
 import SearchGame from '../pages/SearchGame';  // 게임 검색 화면
 import TurkeyDiceOptions from '../pages/games/TurkeyDice/GameOptions';  // 터키다이스 옵션 선택 화면
 import ScoreBoard from '../pages/games/TurkeyDice/ScoreBoard';  // 터키다이스 점수 현황 페이지
-import TurkeyDiceResult from '../components/games/GameResult';  // 터키다이스 게임 결과 페이지
+import TurkeyDiceResult from '../pages/games/TurkeyDice/GameResult';  // 터키다이스 게임 결과 페이지
 import HighlightModal from '../pages/HighlightModal'; // 하이라이트 모달
 import Rule from '../pages/Rule.tsx'
 // ...다른 페이지 import
