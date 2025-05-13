64e17eca80b0ccf6247298ed62f0f1aa0368f2b5
kec0126 <kec0126@naver.com>
2025-05-13 11:39:31 +0900

    S12P31E101-410 feat: 하이라이트 영상 로직 수정

--- TurkeyGames/apps/dashboard/src/pages/HighlightModal.tsx
+++ TurkeyGames/apps/dashboard/src/pages/HighlightModal.tsx
@@ -9,7 +9,12 @@ const HighlightModal: React.FC = () => {
   const { gameId } = useParams();
   
   // location.state에서 데이터 가져오기
-  const { qrValue, title } = location.state || {};
+  const { qrValue, title, videoId } = location.state || {};
+  
+  // 백엔드 설정에 맞는 QR 코드 URL 생성
+  const qrCodeUrl = videoId 
+    ? `http://k12e101.p.ssafy.io:9000/download/video/${videoId}`
+    : `http://k12e101.p.ssafy.io:9000/download/video/${gameId}`;
   
   const handleClose = () => {
     navigate(-1); // 이전 페이지로 돌아가기
@@ -19,7 +24,7 @@ const HighlightModal: React.FC = () => {
     <Highlight 
       isOpen={true} 
       onClose={handleClose} 
-      qrValue={qrValue || `https://example.com/games/${gameId}/highlights`} 
+      qrValue={qrValue || qrCodeUrl} 
       title={title || 'QR코드를 인식하면 최고의 플레이 영상을 보실 수 있어요!'} 
     />
   );
