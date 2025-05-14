36b278b1e89ef901668806cef2b1472fd8a31316
rckwon <neo9lee@gmail.com>
2025-05-14 21:38:56 +0900

    S12P31E101-421 feat: 아케이드 기본 화면 구현현

--- TurkeyGames/apps/gameboard/src/pages/turkeyDiceAcadePage/TurkeyDiceAcadePage.tsx
+++ TurkeyGames/apps/gameboard/src/pages/turkeyDiceAcadePage/TurkeyDiceAcadePage.tsx
@@ -1,30 +1,31 @@
-import React from 'react'
-import styles from './TurkeyDiceAcadePage.module.css'
-import ArcadeMap from '../../assets/images/turkey_acade_map.png'
-import ArcadeScoreCard from '../../components/turkeyDice/Arcade/TurkeyDiceScoreCard'
+// TurkeyDiceAcadePage.jsx
+import React from 'react';
+import styles from './TurkeyDiceAcadePage.module.css';
+import ArcadeMap from '../../assets/images/turkey_acade_map.png';
+import ArcadeScoreCard from '../../components/turkeyDice/Arcade/TurkeyDiceScoreCard';
 
 export default function TurkeyDiceAcadePage() {
-    return (
-      <div className={styles.container}>
-        <div className={styles.CardContainer}>
-          <div className={styles.upside}>
-            <ArcadeScoreCard />
-          </div>
-          <div className={styles.downside}>
-            <ArcadeScoreCard />
-          </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.CardContainer}>
+        <div className={styles.upside}>
+          <ArcadeScoreCard />
         </div>
-        <div>
-          <img src={ArcadeMap} alt="Arcade Map" />
-        </div>
-        <div className={styles.CardContainer}>
-          <div className={styles.upside}>
-            <ArcadeScoreCard />
-          </div>
-          <div className={styles.downside}>
-            <ArcadeScoreCard />
-          </div>
+        <div className={styles.downside}>
+          <ArcadeScoreCard />
         </div>
       </div>
-    )
+      <div className={styles.centerMap}>
+        <img src={ArcadeMap} alt="game map" />
+      </div>
+      <div className={styles.CardContainer}>
+        <div className={styles.upside}>
+          <ArcadeScoreCard />
+        </div>
+        <div className={styles.downside}>
+          <ArcadeScoreCard />
+        </div>
+      </div>
+    </div>
+  );
 }
